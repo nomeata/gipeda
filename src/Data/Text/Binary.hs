dfd48be7a9bd2847c27080bb972a7a8288a29e5b
Joachim Breitner <mail@joachim-breitner.de>
2015-05-26 11:47:12 +0200

    Make Data.Text.Binary actually work

--- src/Data/Text/Binary.hs
+++ src/Data/Text/Binary.hs
@@ -3,10 +3,17 @@ module Data.Text.Binary where
 
 #if MIN_VERSION_text(1,2,1)
 #else
+
+import Development.Shake.Classes
+
+import Data.Text
+import Data.Text.Encoding
+import Data.Functor
+
 -- I do not want a new dependency just for these, so this is copied from text-binary
-instance Binary T.Text where
-    put = put . T.encodeUtf8
-    get = T.decodeUtf8 <$> get
+instance Binary Text where
+    put = put . encodeUtf8
+    get = decodeUtf8 <$> get
 #endif
 
 
