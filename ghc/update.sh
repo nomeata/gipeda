89ab775319ca49fd4be1ba47bc4626ea219a9b6a
Joachim Breitner <mail@joachim-breitner.de>
2015-05-31 21:16:42 +0000

    Adjust update and log2csv for new host

--- ghc/update.sh
+++ ghc/update.sh
@@ -2,6 +2,9 @@
 
 cd $(dirname $0)
 
+export PATH=~/.bin:"$PATH"
+
 git -C repository fetch origin "+refs/heads/*:refs/heads/*" --prune
+git -C logs fetch origin "+refs/heads/*:refs/heads/*" --prune
 ./gipeda -j4
-rsync -rvaiz --delete site/ deb.haskell.org:deb.haskell.org/speed/
+lftp sftp://ghc-perf:xxx@www-origin.haskell.org -e 'mirror -c -e -R site/ ghc-perf/ghc; exit'
