f92ce2f709349260359ee426739dc40063fea426
Joachim Breitner <mail@joachim-breitner.de>
2016-03-31 23:11:46 +0200

    Undo sort by branch, needs more work than 10 minutes of free time

--- site/js/gipeda.js
+++ site/js/gipeda.js
@@ -226,6 +226,7 @@ Handlebars.registerHelper('humanDate', function(timestamp) {
 Handlebars.registerHelper('each_naturally', function(context,options){
     var output = '';
     if (context) {
+	console.log(context);
 	var keys = jQuery.map(context, function(v,k) {return k});
 	var sorted_keys = keys.sort(naturalSort);
 	sorted_keys.map(function (k,i) {
@@ -237,6 +238,7 @@ Handlebars.registerHelper('each_naturally', function(context,options){
 Handlebars.registerHelper('each_unnaturally', function(context,options){
     var output = '';
     if (context) {
+	console.log(context);
 	var keys = jQuery.map(context, function(v,k) {return k});
         // needs https://github.com/overset/javascript-natural-sort/issues/21 fixed
 	//var sorted_keys = keys.sort(naturalSort).reverse();
@@ -244,19 +246,6 @@ Handlebars.registerHelper('each_unnaturally', function(context,options){
 	sorted_keys.map(function (k,i) {
 	    output += options.fn(context[k], {data: {key: k, index: i}});
 	});
-    }
-    return output;
-});
-
-// Sort by age, then by name
-Handlebars.registerHelper('each_branch', function(context,options){
-    var output = '';
-    if (context) {
-	jQuery.map(context, function (b,i) { return {branch: b, branchName: i}; })
-            .sort(function(a,b) { return a.branch.gitDate - b.branch.gitDate; })
-            .map(function (b,i) {
-                output += options.fn(b.branch, {data: {key: b.branchName, index: i}});
-            });
     }
     return output;
 });
