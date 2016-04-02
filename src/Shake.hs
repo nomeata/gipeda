1d8465562d7281c87c8481a14bf497780c3dbead
Sebastian Graf <sgraf1337@gmail.com>
2016-04-02 19:38:47 +0200

    Use the absolute path to the executable instead of './gipeda' for sub commands
    The hope is that it fixes usage of the -C flag and gets rid off the requirement of always having to create a link to the gipeda executable in the working directory.

--- src/Shake.hs
+++ src/Shake.hs
@@ -14,6 +14,7 @@ import System.IO.Extra (newTempFile)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
 import qualified System.Directory
+import System.Environment (getExecutablePath)
 import Data.Aeson
 import qualified Data.Text as T
 
@@ -36,7 +37,8 @@ git gitcmd args = do
 self :: (CmdResult b) => String -> [String] -> Action b
 self name args = do
     -- orderOnly ["gipeda"]
-    cmd (Traced name) "./gipeda" name args
+    gipeda <- liftIO getExecutablePath
+    cmd (Traced name) gipeda name args
 
 gitRange :: Action [String]
 gitRange = do
